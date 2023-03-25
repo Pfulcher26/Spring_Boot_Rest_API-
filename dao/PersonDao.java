9ed10fc886b00f67a1f998e69a3652f65e01e672
pfulcher26 <pfulcher26@gmail.com>
2023-03-24 19:53:26 -0700

    Annotates the PersonService calls with the @Service annotation to instantiate it as a bean to be used across the application.

--- dao/PersonDao.java
+++ dao/PersonDao.java
@@ -1,2 +1,16 @@
-package com.example.demo.dao;public interface PersonDao {
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson (Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
 }
--- model/Person.java
+++ model/Person.java
@@ -1,2 +1,22 @@
-package com.example.demo.model;public class Person {
+package com.example.demo.model;
+
+import java.util.UUID;
+
+public class Person {
+
+    private final UUID id;
+    private final String name;
+
+    public Person(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
