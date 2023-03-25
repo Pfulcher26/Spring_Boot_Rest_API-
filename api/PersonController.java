faa47e97453d65ca19e39ba6fbe8dee38c6596c2
pfulcher26 <pfulcher26@gmail.com>
2023-03-24 20:13:44 -0700

    Annotates the PersonController with the @RequestMapping to indicate that all post requests made to said endpoint that contain a Person object in the req body will map to the addPerson method which invokes the personService.addPerson() method to add the Person object ot the data store.

--- api/PersonController.java
+++ api/PersonController.java
@@ -5,8 +5,10 @@ import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
 
