5125cdca177041f8415290a56b9f9a5195661235
pfulcher26 <pfulcher26@gmail.com>
2023-03-24 21:23:07 -0700

    Implements the updatePersonByID method of the PersonDAo interface.

--- dao/FakePersonDataAccessService.java
+++ dao/FakePersonDataAccessService.java
@@ -43,6 +43,15 @@ public class FakePersonDataAccessService implements PersonDao {
 
     @Override
     public int updatePersonById(UUID id, Person person) {
-        return 0;
+        return selectPersonById(id)
+                .map(p -> {
+                    int indexOfPersonToDelete = DB.indexOf(person);
+                    if (indexOfPersonToDelete >= 0) {
+                        DB.set(indexOfPersonToDelete, person);
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
     }
 }
