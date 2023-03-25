d660e8431d6593730dc34d4e4a8d4d59f27d708b
pfulcher26 <pfulcher26@gmail.com>
2023-03-24 20:02:06 -0700

    Annotates the addPerson method as a post mapping within the PersonController.

--- api/PersonController.java
+++ api/PersonController.java
@@ -3,6 +3,7 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,7 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @PostMapping
     public void addPerson(Person person) {
 
         personService.addPerson(person);
